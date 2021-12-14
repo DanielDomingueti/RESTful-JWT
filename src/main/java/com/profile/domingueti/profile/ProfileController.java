7596f8c1432d3c0a51780a6d46ac4934dd532ed4
DanielDomingueti <danieldomingueti@hotmail.com>
2021-12-14 05:20:14 -0300

    refresh token controller

--- src/main/java/com/profile/domingueti/profile/ProfileController.java
+++ src/main/java/com/profile/domingueti/profile/ProfileController.java
@@ -3,14 +3,23 @@ package com.profile.domingueti.profile;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-import java.lang.ModuleLayer.Controller;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,13 +29,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.profile.domingueti.assembler.ProfileAssembler;
+import com.profile.domingueti.roles.Role;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping(path = "/api")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ProfileController {
 
 	private final ProfileRepository repo;
@@ -65,6 +83,44 @@ public class ProfileController {
 			});
 		EntityModel<Profile> entityModel = assembler.toModel(updatedProfile);
 		return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
+	}
+	
+	@GetMapping("/token/refresh")
+	public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws JsonMappingException, IOException {
+		String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+		if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+			try {
+				String refresh_token = authorizationHeader.substring("Bearer ".length());
+				Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+				JWTVerifier verifier = JWT.require(algorithm).build();
+				DecodedJWT decodedJWT = verifier.verify(refresh_token);
+				String username = decodedJWT.getSubject();
+				Profile user = repo.findByUsername(username);
+
+				String access_token = JWT.create().withSubject(user.getUsername())
+						.withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+						.withIssuer(request.getRequestURL().toString()).withClaim("roles", user.getRoles()
+								.stream().map(Role::getName).collect(Collectors.toList()))
+						.sign(algorithm);
+				Map<String, String> tokens = new HashMap<>();
+				tokens.put("access_token", access_token);
+				tokens.put("refresh_token", refresh_token);
+				response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+				new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+			} catch (Exception e) {
+				log.error("Error loggin in: {}", e.getMessage());
+				response.setHeader("error", e.getMessage());
+				response.setStatus(HttpStatus.FORBIDDEN.value());
+//				response.sendError(HttpStatus.FORBIDDEN.value());
+
+				Map<String, String> error = new HashMap<>();
+				error.put("error_message", e.getMessage());
+				response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+				new ObjectMapper().writeValue(response.getOutputStream(), error);
+			}
+		} else {
+			throw new RuntimeException("Refresh token is missing");
+		}
 	}
 	
 }
